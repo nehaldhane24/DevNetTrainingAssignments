30cd0fce5871ffee26d6e2606d4d8afedef739f5
nehaldhane24 <35840672+nehaldhane24@users.noreply.github.com>
2020-06-02 08:46:24 +0530

    Update test.py

--- DevNetTrainingAssignments-master/data/test.py
+++ DevNetTrainingAssignments-master/data/test.py
@@ -52,6 +52,39 @@ class TestMyFunctions(unittest.TestCase):
  
         
 unittest.main()
-         
+'''
+output :
+
+(base) D:\DevNetTrainingAssignments-master>C:/Users/Home/Miniconda3/python.exe d:/DevNetTrainingAssignments-master/data/test.py
+Printing data from json file :
+{'ACCT100': {'paid': 60, 'due': 100}, 'ACCT200': {'paid': 70, 'due': 60}, 'ACCT300': {'paid': 0, 'due': 0}}
+-------------------------------------------------
+Printing data from xml file :
+ACCT400 {'extra': 'fun'}
+paid 600
+due 10000
+
+ACCT500 {}
+paid 70
+due 40
+
+ACCT600 {}
+paid 0
+due 0
+
+-------------------------------------------------
+Printing data from yml file :
+{'ACCT700': {'paid': 60, 'due': 100}, 'ACCT800': {'paid': 70, 'due': 60}, 'ACCT900': {'paid': 0, 'due': 0}}
+Printing data from json file :
+.Printing data from yml file :
+.
+----------------------------------------------------------------------
+Ran 2 tests in 0.002s
+
+OK
+
+(base) D:\DevNetTrainingAssignments-master>
+
+'''
 
         
