6bb194fb524a7e3147414526b980835fb6741cac
nehaldhane24 <35840672+nehaldhane24@users.noreply.github.com>
2020-06-02 08:47:41 +0530

    Update data_parse_script.py

--- DevNetTrainingAssignments-master/data/data_parse_script.py
+++ DevNetTrainingAssignments-master/data/data_parse_script.py
@@ -1,7 +1,7 @@
 import json
 import xml.etree.ElementTree as ET
 import yaml
-import unittest
+
 
 def parse_json_file():
     print("Printing data from json file : ")
@@ -21,14 +21,43 @@ def parse_xml_file():
                print(j.tag,int(j.text))
            print()
        print("-------------------------------------------------")
+      
 def parse_yml_file():
     print("Printing data from yml file : ")
     with open('D:/DevNetTrainingAssignments-master/data/db.yml','r') as ymlfile : #open the yml file in read mode
-        data = yaml.load(ymlfile) #retreive the data in yml object format
-        print(data) #print the retreived data 
+        data = yaml.safe_load(ymlfile) #retreive the data in yml object format
+        #print(data) #print the retreived data 
+        return data
 
 print(parse_json_file())
 print("-------------------------------------------------")
-
 parse_xml_file()
-parse_yml_file()
+print(parse_yml_file())
+
+'''
+output:
+
+Printing data from json file :
+{'ACCT100': {'paid': 60, 'due': 100}, 'ACCT200': {'paid': 70, 'due': 60}, 'ACCT300': {'paid': 0, 'due': 0}}
+-------------------------------------------------
+Printing data from xml file :
+ACCT400 {'extra': 'fun'}
+paid 600
+due 10000
+
+ACCT500 {}
+paid 70
+due 40
+
+ACCT600 {}
+paid 0
+due 0
+
+-------------------------------------------------
+Printing data from yml file :
+d:/DevNetTrainingAssignments-master/data/data_parse_script.py:27: YAMLLoadWarning: calling yaml.load() without Loader=... is deprecated, as the default Loader is unsafe. Please read https://msg.pyyaml.org/load for full details.
+  data = yaml.load(ymlfile) #retreive the data in yml object format
+{'ACCT700': {'paid': 60, 'due': 100}, 'ACCT800': {'paid': 70, 'due': 60}, 'ACCT900': {'paid': 0, 'due': 0}}
+
+'''
+
