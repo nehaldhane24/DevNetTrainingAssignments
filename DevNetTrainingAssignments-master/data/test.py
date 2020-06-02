ed2c136fee2ab815dfe39900fbed11a3b00d51c0
nehaldhane24 <35840672+nehaldhane24@users.noreply.github.com>
2020-06-02 08:43:38 +0530

    Update test.py

--- DevNetTrainingAssignments-master/data/test.py
+++ DevNetTrainingAssignments-master/data/test.py
@@ -1,5 +1,8 @@
 import data_parse_script
-import unittest
+import io
+import unittest 
+import json
+import parser
 #from xmlunittest import XmlTestCase
 import xml.etree.ElementTree as ET
 class TestMyFunctions(unittest.TestCase):
@@ -19,49 +22,36 @@ class TestMyFunctions(unittest.TestCase):
                         }
                       } #hardcoded json file
 
-        copy_json = json.dump(copy_json, ensure_ascii=False)
-        self.assertEqual(parse_json_file(), copy_json) #check if the loaded file and hardcoded files match
-
-        def test_read_file_data_yml(self):
-            copy_yml =  { 
-                            "ACCT100": {
-                                "paid": 60,
-                                "due": 100
-                            },
-                            "ACCT200": {
-                                "paid": 70,
-                                "due": 60
-                            },
-                            "ACCT300": {
-                                "paid": 0,
-                                "due": 0
-                            }
-                        }  #hardcoded json file
-
-            self.assertEqual(parse_yml_file(), copy_yml) #check if the loaded file and hardcoded files match
-         
-
-        def test_read_file_data_xml(self):
-            copy_xml =  '''<?xml version="1.0" encoding="UTF-8" ?>
-                            <root>
-                            <ACCT400 extra="fun">
-                                <paid>600</paid>
-                                <due>10000</due>
-                            </ACCT400>
-                            <ACCT500>
-                                <paid>70</paid>
-                                <due>40</due>
-                            </ACCT500>
-                            <ACCT600>
-                                <paid>0</paid>
-                                <due>0</due>
-                            </ACCT600>
-                            </root>
-                        '''
-            #hardcoded json file
-            myroot = ET.fromstring(copy_xml)
-            self.assertEqual(parse_xml_file(), myroot) #check if the loaded file and hardcoded files match
-
+        #copy_json = json.dump(copy_json)
+        self.assertEqual(data_parse_script.parse_json_file(), copy_json) #check if the loaded file and hardcoded files match
+   
+    
+        
+    def test_read_file_data_yml(self):
+        copy_yml =  { 
+                        "ACCT700": {
+                            "paid": 60,
+                            "due": 100
+                        },
+                        "ACCT800": {
+                            "paid": 70,
+                            "due": 60
+                        },
+                        "ACCT900": {
+                            "paid": 0,
+                            "due": 0
+                        }
+                    }  #hardcoded json file
+        
+        data1 = json.dumps(data_parse_script.parse_yml_file())
+        copy_yml = json.dumps(copy_yml)
+        #print((data1))
+        #print((copy_yml))
+        self.assertEqual(data1, copy_yml) #check if the loaded file and hardcoded files match
+       
+ 
+        
+unittest.main()
          
 
         
